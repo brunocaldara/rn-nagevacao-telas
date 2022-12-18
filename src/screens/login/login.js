ad9e68667ab4db58365369359cb3bbed142f8f72
Bruno Caldara <brunocaldara@gmail.com>
2022-12-18 20:55:40 -0300

    Reseta a rota quando clica em login

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+import {CommonActions} from '@react-navigation/native';
 
 const LoginScreen = ({navigation}) => {
+  const handleHomeClick = () => {
+    //navigation.navigate('Home');
+    //navigation.dispatch(StackActions.replace('Home', {test: 'Test Params'}));
+
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [{name: 'Home'}],
+      }),
+    );
+  };
+
   return (
     <View>
       <Text>Login Screen</Text>
@@ -9,7 +22,7 @@ const LoginScreen = ({navigation}) => {
         title="Register"
         onPress={() => navigation.navigate('Register')}
       />
-      <Button title="Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Home" onPress={handleHomeClick} />
     </View>
   );
 };
